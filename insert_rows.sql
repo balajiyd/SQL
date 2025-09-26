4427bfd589f1879c18d451eacae39c66dcdb77bd
balajiyd <124142672+balajiyd@users.noreply.github.com>
2025-09-26 10:44:50 +0530

    Create insert_rows.sql

---
+++ insert_rows.sql
@@ -0,0 +1,6 @@
+insert into students
+values
+(192211253, 'Y. Balaji', 9008439331,'2022-09-23'),
+(192211242,'K.Vishnu', 6380462811, '2022-09-30'),
+(192211307,'K.Venkat Sai', 9003322123, '2022-09-22');
+select * from students;
