0fb9c2a8336db120f873c2ee51cd760d17455bcd
balajiyd <124142672+balajiyd@users.noreply.github.com>
2025-09-26 10:46:52 +0530

    Update insert_rows.sql

--- insert_rows.sql
+++ insert_rows.sql
@@ -4,3 +4,12 @@ values
 (192211242,'K.Vishnu', 6380462811, '2022-09-30'),
 (192211307,'K.Venkat Sai', 9003322123, '2022-09-22');
 select * from students;
+
+
+// Select rows
+
+insert into students(student_id,student_name)
+values
+(192211251, 'Tharun');
+
+select * from students;
